6f2d1900cdf97f0430f94ceb1988d1ff506022f7
Soham Roy <soham3.1415@gmail.com>
2018-02-07 20:06:33 -0600

    Removed power and cleaned up code

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,22 +8,22 @@ public class Calculator {
 	public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter expression:: ");
-        String expression = input.nextLine();
+        StringBuilder expression = new StringBuilder(input.nextLine());
         input.close();
-        expression=expression.replace('[', '(');
-        expression=expression.replace(']', ')');
-        String[] splitExpression = expression.split(" ");
-        expression = "";
+        expression = new StringBuilder(expression.toString().replace('[', '('));
+        expression = new StringBuilder(expression.toString().replace(']', ')'));
+        String[] splitExpression = expression.toString().split(" ");
+        expression = new StringBuilder();
         for (String term:splitExpression)
-        		expression+=term;
-        System.out.println(solvePostfix(toPostfix(expression)));
+        		expression.append(term);
+        System.out.println(solvePostfix(toPostfix(expression.toString())));
 	}
 	
 	private static Queue<String> toPostfix(String expression) {
-		String operators = "-+*/^()";
+		String operators = "-+*/()";
 		String[] tokens = expression.split("(?<=["+operators+"])|(?=["+operators+"])");
-		Stack<String> operatorStack = new Stack<String>();
-		Queue<String> output = new LinkedList<String>();
+		Stack<String> operatorStack = new Stack<>();
+		Queue<String> output = new LinkedList<>();
 		for (String token:tokens) {
 			if (isDouble(token))
 				output.offer(token);
@@ -50,18 +50,12 @@ public class Calculator {
 		return output;
 	}
 	
-	public static boolean computeFirst(String operator1, String operator2) {
-		if(getPemdasOrder(operator1) > getPemdasOrder(operator2))
-			return true;
-		else if (getPemdasOrder(operator1) < getPemdasOrder(operator2))
-			return false;
-		else
-			return isLeftAssociative(operator1);
+	private static boolean computeFirst(String operator1, String operator2) {
+		return getPemdasOrder(operator1) > getPemdasOrder(operator2) || getPemdasOrder(operator1) >= getPemdasOrder(operator2) && isLeftAssociative(operator1);
 	}
 	
-	public static boolean isLeftAssociative(String operator1) {
+	private static boolean isLeftAssociative(String operator1) {
 		switch (operator1.charAt(0)) {
-			case '^': return false;
 			case '+': return true;
 			case '-': return true;
 			case '/': return true;
@@ -70,9 +64,8 @@ public class Calculator {
 		}
 	}
 	
-	public static int getPemdasOrder(String operator) {
+	private static int getPemdasOrder(String operator) {
 		switch(operator.charAt(0)) {
-			case '^': return 3;
 			case '*': return 2;
 			case '/': return 2;
 			case '+': return 1;
@@ -82,8 +75,8 @@ public class Calculator {
 		}
 	}
 	
-	public static double solvePostfix(Queue<String> expression) {
-		Stack<Double> postStack = new Stack<Double>();
+	private static double solvePostfix(Queue<String> expression) {
+		Stack<Double> postStack = new Stack<>();
 		while(!expression.isEmpty()) {
 			String term = expression.poll();
 			if(isDouble(term))
@@ -97,15 +90,15 @@ public class Calculator {
 					case '*': postStack.push(a*b); break;
 					case '/': if(b==0)
 							  	throw new IllegalArgumentException("Cannot divide by 0.");
-								postStack.push(a/b); break;
-					case '^': postStack.push(Math.pow(a, b)); break;
+							  postStack.push(a/b); break;
+					default: throw new IllegalArgumentException("Operator not supported.");
 				}
 			}
 		}
 		return postStack.pop();
 	}
 	
-	public static boolean isDouble (String input) {
+	private static boolean isDouble(String input) {
 		try {
 			Double.parseDouble(input);
 			return true;
